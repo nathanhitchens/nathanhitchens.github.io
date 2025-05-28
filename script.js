373652db57bb1b608c796cdc4eb566d08323edb0
Nathan Hitchens <nmh84@drexel.edu>
2025-05-28 16:16:13 -0400

    removing files for fresh start

--- script.js
+++
@@ -1,7 +0,0 @@
-function changeImage() {
-    document.getElementById("gunjanimage").setAttribute("src", "images/gunjanhit.JPG");
-    
-    setTimeout(() => {
-        document.getElementById("gunjanimage").setAttribute("src", "images/gunjanstand.JPG");
-    }, 250);
-}
